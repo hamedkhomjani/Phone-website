666d07d2e3faa8354773b98285e84ab3876da677
Hamed Khomjani <64583620+hamedkhomjani@users.noreply.github.com>
2020-10-11 17:30:36 +0200

    10

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,13 +1,1 @@
 "use strict";
-
-var circle = document.querySelector("circle");
-var upBtn = document.querySelector("upBtn");
-var downBtn = document.querySelector("downBtn");
-var rotateValue = circle.style.transform;
-var rotateSum;
-
-upBtn.onclick = function () {
-  rotateSum = rotateValue + "rotate(-90deg)";
-  circle.style.transform = rotateSum;
-  rotateValue = rotateSum;
-};
