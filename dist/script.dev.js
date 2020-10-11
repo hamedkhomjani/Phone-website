d146d1caddebcd7bf3f8e7e90381ab42f2d90f99
Hamed Khomjani <64583620+hamedkhomjani@users.noreply.github.com>
2020-10-11 17:07:42 +0200

    8

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -3,3 +3,4 @@
 var circle = document.querySelector("circle");
 var upBtn = document.querySelector("upBtn");
 var downBtn = document.querySelector("downBtn");
+var rotateValue = circle.style.transform;
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 var circle = document.querySelector("circle");
 var upBtn = document.querySelector("upBtn");
 var downBtn = document.querySelector("downBtn");
+
+var rotateValue = circle.style.transform;
